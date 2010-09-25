63529a0c94297a9320820e714d0573bd5e6c75ab
irfanevrens <irfanevrens@2fb2a8e0-2fb1-11df-bfeb-7b4c664f286a>
2010-09-25 22:53:39 +0000

    iletişim formunda bazı kod değişiklikleri yapıldı hata ve bilgilendirme mesajları için eburhan.com css stili entegre edildi.

--- trunk/iletisim-formu.php
+++ trunk/iletisim-formu.php
@@ -1,3 +1,67 @@
+<?php 
+
+// form doldurulup gönderildi mi?
+if(isset($_POST['submit'])) {
+	
+	// evet, o halde form bilgilerini işlemeye başla
+	
+	// POST ile gönderilern verileri aldık
+	$ad 		= trim($_POST["ad"]);
+	$soyad		= trim($_POST["soyad"]);
+	$email		= trim($_POST["email"]);
+	$meslek		= trim($_POST["meslek"]);
+	$telefon	= trim($_POST["telefon"]);
+	$egitim		= trim($_POST["egitim"]);
+	$gmesaj		= trim($_POST["gmesaj"]);
+	
+	// hata kontrollerine başladık
+	try {
+		
+		// Boş alanların kontrolü yapılıyor
+		if (empty($ad)) throw new Exception('Adı alanı boş geçilemez.');
+		if (empty($soyad)) throw new Exception('Soyadı alanı boş geçilemez.');
+		if (empty($email)) throw new Exception('Mail alanı boş geçilemez.');
+		if (empty($gmesaj)) throw new Exception('Mesaj alanı boş geçilemez.');
+		
+		// mail gönderme işlemini başlatabiliriz.
+		$to = 'bilgi@buraya.com';
+		$konu = "İletişim Formundan Gönderilen Mesaj";           
+		$mesaj = 'Yeni bir iletişim haberi geldi <br />
+		<br />
+		<b>Ad</b> : '.$ad.'<br />
+		<b>Soyad</b> : '.$soyad.'<br />
+		<b>E-Mail</b> : <a href="mailto:'.$email.'">'.$email.'</a><br />
+		<b>Meslek</b> : '.$meslek.'<br>
+		<b>Telefon</b> : '.$telefon.'<br />
+		<b>Eğitim</b> : '.$egitim.'<br />
+		<b>Mesaj</b> : '.$gmesaj.'
+		';	
+	
+		// headerlar mailin düzgün ulaşması için gereklidir.
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=UTF-8"' . "\r\n";
+		$headers .= 'To: Web Siteniz <sercan@bulsam.net>' . "\r\n";
+		$headers .= 'From: '.$ad.' <'.$email.'>' . "\r\n";
+		
+		// mail gönder
+		if (mail($to,$konu,$mesaj,$headers)) {
+			
+			$tamam = 'Mesajınız alındı, en kısa zamanda incelenecektir.';
+		} else {
+			
+			throw new Exception('Mesaj gönderilirken hata meydana geldi. Lütfen daha sonra yeniden deneyiniz.');
+		}
+	} catch (Exception $ex) {
+		
+		$hata = $ex->getMessage();
+	}
+} else {
+	
+	// hayır
+}
+
+?>
+
 <html>
 	<head>
 		
@@ -6,6 +70,7 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		
 		<link href="css/iletisim-formu.css" rel="stylesheet" type="text/css" />
+		<link href="css/eburhan.css" rel="stylesheet" type="text/css" />
 
 	</head>
 
@@ -13,105 +78,66 @@
 		<div id="genel">
 		İletişim Formu - Burası Başlıktır
 			<div id="bolum">
-				<form method="POST" action="<?php echo $_SERVER['SCRIPT_FILENAME'] ?>">
-					<table>
+				<form method="POST" action="<?php echo $_SERVER['SCRIPT_NAME'] ?>">
+					<table width="100%">
 						<tr>
-							<td></td>
-							<td>
+							<td colspan="2">
 							<?php
-							//POST ile gönderilern verileri çektik
-								$ad=$_POST["ad"];
-								$soyad=$_POST["soyad"];
-								$email=$_POST["email"];
-								$meslek=$_POST["meslek"];
-								$telefon=$_POST["telefon"];
-								$egitim=$_POST["egitim"];
-								$onay=$_POST["onay"];
-								$gmesaj=$_POST["mesaj"];
-								
-								
-							if($_POST) {
-							
-								// Boş alanları belirledik hata bildirdik
-								if(empty($ad)or empty($soyad) or empty($meslek) or empty($telefon) or empty($egitim) or empty($onay)or empty($gmesaj)) {echo "Boş alanları doldurunuz";}
-								
-								//email adresini kontrol ediyoruz
-								
-								else if(!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)) {echo "Geçerli bir e-mail adresi girin";}
-								
-								//telefon numarasını kontrol ediyoruz
-								
-								else if (!ctype_digit($telefon)) {echo "Geçerli bir telefon numarası girin";}
-								
-								//kendi adresimize mail gönderelim
-								else {
-										
-										$konu = "İletişim Formundan Gönderilen Mesaj";           
-										$mesaj = 'Yeni bir iletişim haberi geldi <br><br>
-										
-										<b>Ad</b> : '.$ad.'<br>
-										<b>Soyad</b> : '.$soyad.'<br>
-										<b>E-Mail</b> : <a href="mailto:'.$email.'">'.$email.'</a><br>
-										<b>Meslek</b> : '.$meslek.'<br>
-										<b>Telefon</b> : '.$telefon.'<br>
-										<b>Eğitim</b> : '.$egitim.'<br>
-										<b>Onay</b> : '.$onay.'<br>
-										<b>Mesaj</b> : '.$gmesaj.'<br>
-										';	
-									
-										// headerlar mailin düzgün ulaşması için gereklidir.
-										$headers  = 'MIME-Version: 1.0' . "\r\n";
-										$headers .= 'Content-type: text/html; charset=UTF-8"' . "\r\n";
-										$headers .= 'To: Web Siteniz <sercan@bulsam.net>' . "\r\n";
-										$headers .= 'From: '.$ad.' <'.$email.'>' . "\r\n";
-									   
-									 //mail bilgileri alındıktan sonra göndeririyoruz..
-									if ( mail($to,$konu,$mesaj,$headers) ) {echo "İletişim bilgileriniz alındı"; }
-									   
-									else {echo "İletişim bilgileri gönderilemedi"; }
 
-									
-									}
+							if(isset($_POST['submit'])) {
 								
+								if (!empty($tamam)) { 
+									
+									echo '<div class="tamam">' . $tamam . '</div>';
+								} else {
+									
+									if (!empty($hata)) echo '<div class="hata">' . $hata . '</div>';
 								}
+							} else {
+								
+								$ad 		= '';
+								$soyad		= '';
+								$email		= '';
+								$meslek		= '';
+								$telefon	= '';
+								$egitim		= '';
+								$gmesaj		= '';
+							}
+			
 							?>
 							</td>
 						</tr>
 						<tr>
-							<td>Adınız :</td>
-							<td><input type="text" class="box" name="ad"></td>
+							<td>* Ad :</td>
+							<td><input type="text" class="box" name="ad" value="<?=$ad?>"></td>
 						</tr>
 						<tr>
-							<td>Soyadınız :</td>
-							<td><input type="text" class="box" name="soyad"></td>
+							<td>* Soyad :</td>
+							<td><input type="text" class="box" name="soyad" value="<?=$soyad?>"></td>
 						</tr>
 						<tr>
-							<td>Email :</td>
-							<td><input type="text" class="box" name="email"></td>
+							<td>* Mail Adresi :</td>
+							<td><input type="text" class="box" name="email" value="<?=$email?>"></td>
 						</tr>
 						<tr>
-							<td>Mesleğniz :</td>
-							<td><input type="text" class="box" name="meslek"></td>
+							<td>Meslek :</td>
+							<td><input type="text" class="box" name="meslek" value="<?=$meslek?>"></td>
 						</tr>
 						<tr>
 							<td>Telefon :</td>
-							<td><input type="text" class="box" name="telefon"></td>
-						</tr>
+							<td><input type="text" class="box" name="telefon" value="<?=$telefon?>"></td>
 						</tr>
 						<tr>
 							<td>Eğitim :</td>
-							<td><input type="text" class="box" name="egitim"></td>
+							<td><input type="text" class="box" name="egitim" value="<?=$egitim?>"></td>
 						</tr>
 						<tr>
-							<td>Onay :</td>
-							<td><select class="box" name="onay"><option value="evet">Evet</option><option value="hayır">Hayır</option></select></td>
-						<tr>
-							<td>Mesaj :</td>
-							<td><textarea class="text_area" name="mesaj"></textarea></td>
-						</tr>	
+							<td>* Mesaj :</td>
+							<td><textarea class="text_area" name="gmesaj"><?=$gmesaj?></textarea></td>
+						</tr>
 						<tr>
 							<td></td>
-							<td><input type="submit" value="Gönder" class="btn"></td>
+							<td><input type="submit" name="submit" value="Gönder" class="btn"></td>
 						</tr>	
 					</table>
 				</form>
